08668846f8d245192d5b8121eecd333e07f7a1af
experimeantal218 <experimeantal218@gmail.com>
2025-08-14 20:30:36 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/inner-temple-galaxy-prototype/', // ← matches your repo name
+  base: '/inner-temple-galaxy-prototype/', // must match your repo name
 })
